2e67c7eee8ec8c13e01a3142d97e2909eb087b87
Hofls <pleshakov@proitr.ru>
2020-08-06 22:18:23 +0500

    Nodejs. Date

--- nodejs/snippets/date.js
+++ nodejs/snippets/date.js
@@ -10,3 +10,9 @@ function stringToDate() {
 function dateToString() {
     let dateString = moment(new Date()).format('DD-MM-YYYY');
 }
+
+function daysDifference() {
+    let momentA = moment("01.10.2015", "DD.MM.YYYY");
+    let momentB = moment("12.11.2015", "DD.MM.YYYY");
+    let duration = moment.duration(momentB.diff(momentA)).asDays();
+}
