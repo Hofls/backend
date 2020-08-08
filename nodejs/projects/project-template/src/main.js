455d05d9847b619841a78a24554d62bd4ea625f9
Hofls <pleshakov@proitr.ru>
2020-08-08 18:01:00 +0500

    NodeJS template. Config example

--- nodejs/projects/project-template/src/main.js
+++ nodejs/projects/project-template/src/main.js
@@ -1,4 +1,8 @@
 const messenger = require("./messenger");
+var config = require('../config/dev-config.json');
+
+
 
 console.log(messenger.getMessage());
 console.log(`Current profile is ${process.env.NODE_PROFILE}`);
+console.log(`Value from config - ${config.dbName}`);
