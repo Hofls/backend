37d08aad95d8559b03361d59f2c86544bddc52f8
hofls <hofls@ya.ru>
2021-11-27 16:35:56 +0500

    kotlin basics

---
+++ kotlin/kotlin.kts
@@ -0,0 +1,41 @@
+// Haven't used Kotlin language for a long time? Here is a quick reminder:
+
+// Data types
+var count = 5
+val weight = 3.5
+var message = "Hello world"
+val isReady = true
+
+// Variable in string
+println("Hello $userName")
+
+// Collections
+
+// Null safety
+var address = bob?.department?.head?.address
+
+var name: String? = null
+if (name != null) {
+    println(name) // Won't be prited
+}
+
+// Functions
+fun multiply(number: Int): Int {
+    return number * 3
+}
+multiply(2)
+
+// Conditions
+if (true) {
+    println("A")
+} else {
+    println("B")
+}
+
+// Cycles
+
+// Exceptions
+
+// Modules (Imports)
+
+// Classes
