8177933852c3f0d97e5e059291dca38888d58e04
hofls <hofls@ya.ru>
2022-06-06 19:38:24 +0500

    python - monitoring example

---
+++ python/projects/vpn-monitoring/monitor.py
@@ -0,0 +1,29 @@
+# To run:
+# crontab -e
+# */15 * * * * python3 /opt/monitoring/monitor.py
+
+import os
+import time
+
+def isResourceAvailable(container):
+    result = os.popen(f"docker exec {container} curl https://example.com").read()
+    return "Example Domain" in result
+
+def vpnIsWorking():
+    if (not isResourceAvailable("crawler")):
+        return False
+    if (not isResourceAvailable("notifier")):
+        return False
+    return True
+
+def restartContainers():
+    os.system("docker restart fortivpn")
+    time.sleep(10)
+    os.system("docker restart crawler")
+    os.system("docker restart notifier")
+
+if (vpnIsWorking()):
+    print("Everything is OK")
+else:
+    print("Restart all containers")
+    restartContainers()
