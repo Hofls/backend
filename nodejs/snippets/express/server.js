e38f86289549a5cb0d67e14bb0aadc5bb1b7ea7c
hofls <hofls@ya.ru>
2022-11-25 17:31:32 +0500

    express warning

--- nodejs/snippets/express/server.js
+++ nodejs/snippets/express/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
+// Warning! Express is able to serve only one request at a time (if it takes 10 seconds, all other requests have to wait)
 module.exports = {
 
     run: function() {
         const app = express();
         app.use(express.json());
 
-        app.get("/", (req, res) => {
+        app.get("/", async (req, res) => {
             res.send("Try to send POST request");
         });
 
-        app.post("/", (req, res) => {
+        app.post("/", async (req, res) => {
             res.send(req.body); // if body is empty, try to set "Content-Type: application/json" on client
         });
 
