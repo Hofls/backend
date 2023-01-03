52ef534f7b09d5449f0d6fda6f14b3390e4a3885
hofls <hofls@ya.ru>
2023-01-03 16:42:23 +0500

    go - postgres

--- go/projects/postgres/main.go
+++ go/projects/postgres/main.go
@@ -20,20 +20,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
 	// INSERT
-	err = db.QueryRow(`INSERT INTO customer(name, rank) VALUES($1, $2)`, "John", 3).Err()
+	_, err = db.Exec(`INSERT INTO customer(name, rank) VALUES($1, $2)`, "John", 3)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// UPDATE
-	err = db.QueryRow(`UPDATE customer SET name = $1, rank = $2 WHERE id = 1`, "Sally", 15).Err()
+	_, err = db.Exec(`UPDATE customer SET name = $1, rank = $2 WHERE id = 1`, "Sally", 15)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// SELECT
+	// SELECT (1 ROW)
+	var count string
+	err = db.QueryRow("SELECT COUNT(*) FROM customer WHERE name <> $1", "Leslie").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Count - " + count)
+
+	// SELECT (MULTIPLE ROWS)
 	rows, err := db.Query("SELECT * FROM customer WHERE name <> $1", "Leslie")
 	if err != nil {
 		log.Fatal(err)
